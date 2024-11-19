194049e3ea5163cceb8dc16640eed73c8184034f
Romano-Gabriele <romano.gabriele@istitutoagnelli.it>
2024-11-20 00:10:32 +0100

    - aggiunta esercitazione 11

--- romano_gabriele/svelte/WebApp/src/routes/input/+page.svelte
+++ romano_gabriele/svelte/WebApp/src/routes/input/+page.svelte
@@ -1,1 +1,23 @@
+<script>
+    import { bind } from "svelte/internal";
+
+    let nome, cognome, eta, residenza;
+    let confermato = false;
+</script>
+
 <h1>Input Page</h1>
+
+Nome <input bind:value={nome}>
+Cognome <input bind:value={cognome}>
+Età <input bind:value={eta}>
+Residenza <input bind:value={residenza}>
+
+<br><br>
+
+Confermato <input type="checkbox" bind:checked={confermato}>
+
+<p>
+    Io sono {nome} {cognome} ed ho {eta} anni e abito a {residenza}
+</p>
+
+<button disabled={!confermato}>Invia</button>
