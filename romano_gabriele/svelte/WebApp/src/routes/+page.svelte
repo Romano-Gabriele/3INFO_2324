7249bff34eef3b737b3bf8383c57e1ac69eaba3e
Romano-Gabriele <romano.gabriele@istitutoagnelli.it>
2024-11-19 22:17:26 +0100

    - aggiunta progetto WebApp ed esercitazione 08

---
+++ romano_gabriele/svelte/WebApp/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<main>
+    <h1>WELCOME to my WebApp</h1>
+</main>
+
+<style>
+    main {
+        width: 100%;
+        height: 100%;
+        display: grid;
+        justify-content: center;
+        align-items: center;
+        border: 3px solid blue;
+    }
+
+    h1 {
+        font-size: 80px;
+    }
+</style>
