1061e955870256833c6bf7bc6ad974eb17db55e9
Romano-Gabriele <romano.gabriele@istitutoagnelli.it>
2024-11-22 12:43:37 +0100

    - aggiunta esercitazione 13

--- romano_gabriele/svelte/WebApp/src/routes/counter/+page.svelte
+++ romano_gabriele/svelte/WebApp/src/routes/counter/+page.svelte
@@ -4,14 +4,54 @@ import Counter from "../../lib/components/counter.svelte";
     import DoubleCounter from "../../lib/components/double_counter.svelte";
     let contatore = 0;
     let doubleContatore = 0;
+    let numeri = [1, 7, 2, 6, 9, 10]
+    let sottolineato = true;
+    let doppio;
+
+    $: {
+        doppio = contatore * 2;
+        sottolineato = !(contatore % 2);
+    }
 </script>
 
 <h1>Pagina Counter!!!</h1>
 
-<h2>Il valore del contatore vale: {contatore}</h2>
+<!-- {#if contatore < 5}
+    <h2 class="rosso">Il valore del contatore vale: {contatore}</h2>
+{:else}
+    <h2 class="verde">Il valore del contatore vale: {contatore}</h2>
+{/if} -->
+
+<!-- <h2 class={contatore < 5 ? "rosso" : "verde"}>Il valore del contatore vale: {contatore}</h2> -->
+
+<!-- <h2 class:rosso={contatore < 5}>Il valore del contatore vale: {contatore}</h2> -->
+
+<h2 class:sottolineato>Il valore del contatore vale: {contatore}</h2>
+
+<h2>Il suo doppio vale {doppio}</h2>
 
 <Counter bind:counter={contatore}></Counter>
 
 <h2>Il valore del contatore double vale: {doubleContatore}</h2>
 
 <DoubleCounter bind:counter={doubleContatore}></DoubleCounter>
+
+<ul>
+    {#each numeri as numero, indice}
+        <li>numeri[{indice}]:{numero}</li>
+    {/each}
+</ul>
+
+<style>
+    .rosso {
+        color: red;
+    }
+
+    .verde {
+        color: green;
+    }
+
+    .sottolineato {
+        text-decoration: underline;
+    }
+</style>
